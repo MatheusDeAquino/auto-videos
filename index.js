6c0c6716230e014b1c6687f92dd997fd549a82df
Matheus de Aquino <matheusaquinof@gmail.com>
2019-03-21 13:37:22 -0300

    Start text bot

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const readline = require('readline-sync')
+const robots = {
+	text: require('./robots/text.js')
+}
 
-function start() {
+async function start() {
 	const content = {}
 
 	content.searchTerm = askAndReturnSearchTerm()
 	content.prefix = askAndReturnPrefix()
 
+	await robots.text(content)
+
 	function askAndReturnSearchTerm() {
 		return readline.question('Digite um termo de pesquisa da Wikipedia: ')
 	}
+	
 	function askAndReturnPrefix() {
 		const prefixes = ['Quem e', 'O que e', 'A historia de']
 		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma alternativa: ')
