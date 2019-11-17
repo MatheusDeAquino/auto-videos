b6eeaf700226520ab61d74a34d809c9735825b15
Matheus de Aquino <matheusaquinof@gmail.com>
2019-11-17 18:01:11 -0300

    corection of feat: add image download logic with fallback

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const robots = {
 
 async function start() {
 	robots.input()
-	await robots.image()
 	await robots.text()
-
+	await robots.image()
+	
 	const content = robots.state.load()
 	console.dir(content, {depth: null})
 }
--- robots/image.js
+++ robots/image.js
@@ -6,14 +6,16 @@ const state = require('./state.js')
 const googleSearchCredentials = require('../credentials/google-search.json')
 
 async function robot() {
+    
     const content = state.load()
 
     await fetchImagesOfAllSentences(content)
-    await downloadAllImages(content) 
+    await downloadAllImages(content)
 
     state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
+        //console.log(content.sentences)
         for (const sentence of content.sentences) {
             const query = '${content.searchTerm} ${sentence.keywords[0]}'
             sentence.images = await fetchGoogleAndReturnImagesLinks(query)
@@ -28,7 +30,7 @@ async function robot() {
             cx: googleSearchCredentials.searchEngineId,
             q: query,
             searchType: 'image',
-            num: 3
+            num: 2
         })
 
         const imagesUrl = response.data.items.map((item) => {
@@ -39,33 +41,34 @@ async function robot() {
     }
 
     async function downloadAllImages(content) {
-        content.downloadAllImages = []
+        content.downloadedImages = []
 
         for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-            const images = content.sentence[sentenceIndex].images
+            const images = content.sentences[sentenceIndex].images
 
-            for(let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
                 const imageUrl = images[imageIndex]
 
-                try  {
-                    if(content.downloadAllImages.includes(imageUrl)) {
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
                         throw new Error('Imagem já foi baixada')
                     }
 
-                    await downloadAndSave(imageUrl, '$(sentenceIndex)-original.png')
-                    content.downloadAllImages.push(imageUrl)
-                    console.log('> [${sentenceIndex}] [${imageIndex}] Baixou imagem com sucesso: ${imageUrl}')
+                    await downloadAndSave(imageUrl, '${sentenceIndex}-original.png')
+                    content.downloadedImages.push(imageUrl)
+                    console.log('> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}')
                     break
-                } catch(error) {
-                    console.log('> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}') 
+                } catch (error) {
+                    console.log('> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}')
                 }
             }
         }
     }
+
     async function downloadAndSave(url, fileName) {
         return imageDownloader.image({
-          url, url,
-          dest: './content/${fileName}'
+            url, url,
+            dest: './content/${fileName}'
         })
     }
 }
